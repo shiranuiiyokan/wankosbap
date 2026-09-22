25f2cfadead028ece283ac6efbe961ea0a5ff90a
shiranuiiyokan <tukutukutuun@gmail.com>
2026-09-23 08:57:29 +0900

    Add video-worker/drive_client.py

---
+++ video-worker/drive_client.py
@@ -0,0 +1,22 @@
+import json
+import os
+from pathlib import Path
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaIoBaseDownload
+
+SCOPES = ["https://www.googleapis.com/auth/drive"]
+
+def build_drive_service():
+    info = json.loads(os.environ["GOOGLE_SERVICE_ACCOUNT_JSON"])
+    creds = service_account.Credentials.from_service_account_info(info, scopes=SCOPES)
+    return build("drive", "v3", credentials=creds, cache_discovery=False)
+
+def download_file(service, file_id, destination: Path):
+    destination.parent.mkdir(parents=True, exist_ok=True)
+    request = service.files().get_media(fileId=file_id, supportsAllDrives=True)
+    with destination.open("wb") as fh:
+        downloader = MediaIoBaseDownload(fh, request)
+        done = False
+        while not done:
+            _, done = downloader.next_chunk()
